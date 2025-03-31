b9faaa4ced6a4e26824c3f39f24e632ec598e62a
Sandro <sandrodenis373@gmail.com>
2025-03-31 17:16:47 -0300

    fix: adiciona axios e atualiza configuração do Vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3005',
@@ -15,7 +15,18 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    rollupOptions: {
+      external: ['mongodb'],
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'axios']
+        }
+      }
+    }
+  },
   optimizeDeps: {
+    include: ['axios'],
     exclude: ['mongodb']
   }
 })
